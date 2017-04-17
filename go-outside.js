0ae1909b9474f84a4fe041139b245aa11c09d13f
khuram <knshaikh@gmail.comgit config --global user.email knshaikh@gmail.comgit config --global user.email knshaikh@gmail.com>
2017-04-17 00:21:44 +0000

    Adds conditional exercies

---
+++ go-outside.js
@@ -0,0 +1,31 @@
+
+
+
+var temperature = 41;
+var raining = false;
+/*
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you are ready to go outside!");
+
+
+
+if (temperature < -40 || temperature > 40) {
+  console.log("May be going outside isnt such a great idea!");
+}
+
+*/
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}

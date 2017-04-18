eeb69b24455bfc2ddbd6c21fc9c55ad64590b4d6
khuram <knshaikh@gmail.comgit config --global user.email knshaikh@gmail.comgit config --global user.email knshaikh@gmail.com>
2017-04-18 00:33:31 +0000

    Adds looping exercise

---
+++ packaging-list.js
@@ -0,0 +1,6 @@
+var packagingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooding utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (var i = 0; i < packagingList.length; i++) {
+  console.log(packagingList[i]);
+}
---
+++ test.js
@@ -0,0 +1,34 @@
+//Eloquent Javascript exp 1
+// var number = 0;
+
+// while (number <= 12) {
+//   console.log(number);
+//   number++
+// }
+//exp 2
+// var result = 1;
+// var counter = 0;
+
+// while (counter < 10) {
+//   result = result * 2;
+//   counter++;
+// }
+//exp 3
+// console.log(result);
+
+// for (var n = 0; n < 12; n = n + 2)
+// console.log(n);
+
+//example 4
+// var r = 1;
+// for (var c=0; c<10; c = c+1)
+//   r = r * 2;
+//   console.log(r)
+
+//example 5 Breaking out of loop
+
+for (var n = 20; ; n++) {
+  if (n % 7 == 0)
+    break;
+}
+console.log(n);
